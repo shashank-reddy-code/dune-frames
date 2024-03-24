174054a43fa1d993c489174115bcbcbd0fcf4ed2
shashank-reddy-code <shashank.am@outlook.com>
2024-03-23 20:33:33 -0700

    add cast sense basic frame with 30d fid trends

--- api/index.tsx
+++ api/index.tsx
@@ -1,92 +1,163 @@
-import { Button, Frog } from 'frog'
-import { neynar } from 'frog/hubs'
-import { handle } from 'frog/vercel'
-import { getRecommendations } from './dune.js'
-import dotenv from 'dotenv';
+import { Button, Frog } from "frog";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
+import { neynar } from "frog/hubs";
+import { handle } from "frog/vercel";
+import { getFidStats, getRecommendations } from "./dune.js";
+import dotenv from "dotenv";
 dotenv.config();
 
 export const app = new Frog({
-  assetsPath: '/',
-  basePath: '/api',
-  hub: neynar({ apiKey: process.env["NEYNAR_API"] || '' })
-})
+  assetsPath: "/",
+  basePath: "/api",
+  hub: neynar({ apiKey: process.env["NEYNAR_API"] || "" }),
+});
 
-app.frame('/', async (c) => {
-  const { buttonValue, status, frameData, verified } = c
-  const option = buttonValue
-  let recs_list: { key: any; value: any; }[];
-  console.log("loading...", status, option)
-
-  if (status === 'response' && verified) {
-    console.log("running filter", option, frameData?.fid)
-    recs_list = await getRecommendations(frameData?.fid);
-  }
-
+app.frame("/", async (c) => {
   return c.res({
+    action: "/fid30dStats",
     image: (
       <div
         style={{
-          alignItems: 'center',
-          background: 'linear-gradient(to right, #E1E1F9, #FFECEB)',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
+          alignItems: "center",
+          background: "linear-gradient(to right, #E1E1F9, #FFECEB)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
         }}
       >
         <div
           style={{
-            color: 'black',
+            color: "black",
             fontSize: 50,
-            fontStyle: 'normal',
-            letterSpacing: '-0.020em',
+            fontStyle: "normal",
+            letterSpacing: "-0.020em",
             lineHeight: 1.3,
             marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-            display: 'flex',
-            flexDirection: 'column',
-            fontWeight: 'bold'
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+            display: "flex",
+            flexDirection: "column",
+            fontWeight: "bold",
           }}
         >
-          {status === 'response'
-            ? option 
-              ? 
-                recs_list.map((result) => (
-                  <div style={{ textAlign: 'right' }}>
-                    {`${result.key}: ${result.value}`}
-                  </div>))
-              : ''
-            : `See recomended users by persona\n\n 🤗⛓💻📈👟💎🥇👂📡`}
+          Cast Sense: Get a pulse on your Farcaster activity{" "}
         </div>
-        <div
-            style={{
-              position: 'absolute',
-              bottom: '10px',
-              right: '20px',
-              display: 'flex',
-              flexDirection: 'column',
-              width: '150px',
-              height: '80px',
-            }}
-          >
-            <img src="https://assets-global.website-files.com/62fca6954348cf2e3c918eee/64ff2a324de75d7cf9cab050_Dune%20logo.svg" alt="Dune Logo" />
-          </div>
       </div>
     ),
-    intents: [
-      status === 'initial'  && <Button value="followers">See From Your Followers</Button>,
-      status === 'response' && <Button value={option}>Show More</Button>,
-      status === 'response' && <Button.Link href="https://github.com/andrewhong5297/dune-frames">See Frame Code</Button.Link>,
-      // (status === 'initial' || (status === 'response' && option === 'followers')) && <Button value="following">Following You</Button>,
-      // status === 'response' && <Button.Reset>Back</Button.Reset>,
-    ],
-  })
-})
+    intents: [<Button>Continue</Button>],
+  });
+});
 
-export const GET = handle(app)
-export const POST = handle(app)
+app.frame("/fid30dStats", async (c) => {
+  const { status, frameData, verified } = c;
+  let trends_list: { key: any; value: any }[];
+  console.log("loading...", status);
+
+  if (status === "response" && verified) {
+    console.log("running filter", frameData?.fid);
+    trends_list = await getFidStats(frameData?.fid);
+  }
+
+  const types = ["casts", "followers", "likes", "recasts", "mentions"];
+  return c.res({
+    action: "/followerActiveHours",
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background: "linear-gradient(to right, #E1E1F9, #FFECEB)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "black",
+            fontSize: 50,
+            fontStyle: "normal",
+            letterSpacing: "-0.020em",
+            lineHeight: 1.3,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+            display: "flex",
+            flexDirection: "column",
+            fontWeight: "bold",
+          }}
+        >
+          {types.map((type) => (
+            <div
+              key={type}
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                marginBottom: "20px",
+              }}
+            >
+              <span
+                style={{
+                  fontSize: "24px",
+                  fontWeight: "bold",
+                  width: "150px",
+                  paddingRight: "20px", // Added padding to the right of the type
+                }}
+              >
+                {type.charAt(0).toUpperCase() + type.slice(1)}
+              </span>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <span
+                  style={{
+                    fontSize: "36px",
+                    fontWeight: "bold",
+                    marginRight: "10px",
+                  }}
+                >
+                  {trends_list[`current_period_${type}`]}
+                </span>
+                <span
+                  style={{
+                    fontSize: "18px",
+                    color:
+                      trends_list[`${type}_percentage_change`] < 0
+                        ? "#ff4c4c"
+                        : "#29cc97",
+                  }}
+                >
+                  {trends_list[`${type}_percentage_change`] < 0 ? "↓" : "↑"}
+                  {`${Math.abs(
+                    trends_list[`${type}_percentage_change`]
+                  ).toFixed(0)}%`}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    ),
+    intents: [<Button>See more insights</Button>],
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+devtools(app, { serveStatic });
